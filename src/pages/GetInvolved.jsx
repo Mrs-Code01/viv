055f3a6f9b8b3bcf0cc24eb787cf700c856bc6f2
Javascriptbabe <javascriptbabe@gmail.com>
2025-01-31 14:44:20 +0100

    made it responsive

--- src/pages/GetInvolved.jsx
+++ src/pages/GetInvolved.jsx
@@ -29,7 +29,7 @@ const GetInvolved = () => {
     if (showNonProfit !== null) {
       const partnerTimer = setTimeout(() => {
         setShowNonProfit(null)
-      }, 7000)
+      }, 20000)
       return () => clearTimeout(partnerTimer)
     }
   }, [showNonProfit])
